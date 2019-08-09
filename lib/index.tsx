b3c0ca107541f746938cccacd54c3ee4aee3a9ee
walker213 <921254299@qq.com>
2019-08-09 21:38:55 +0800

    完善onClick类型

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import Icon from './icon';
 
+const fn: React.MouseEventHandler = e => {
+	console.log(e.target)
+}
+
 ReactDOM.render(<div>
-	<Icon name='wechat'></Icon>
+	<Icon name='wechat' onClick={fn}></Icon>
 </div>,document.getElementById('root'))
