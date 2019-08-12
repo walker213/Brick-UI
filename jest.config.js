08849ff5aef8abab8d5bb5349bbd8e04db03a2f7
walker213 <921254299@qq.com>
2019-08-12 22:21:01 +0800

    修改jest相关配置

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   moduleFileExtensions: ["js", "json", "jsx", "node", "ts", "tsx"],
   moduleDirectories: ["node_modules"],
   moduleNameMapper: {
-    // mock模块
+    // mock模块, mock非js文件导入
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/tests/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/tests/__mocks__/cssMock.js"
+      "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/cssMock.js"
   },
 
   testMatch: ["<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)"],
