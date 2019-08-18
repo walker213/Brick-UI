4cfa6e99f59f7bfc6e54063647f698db09cfbc0b
walker213 <921254299@qq.com>
2019-08-18 15:20:34 +0800

    改写index.tsx为入口文件导出所有组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = e => {
-	console.log(e.target)
-}
-
-ReactDOM.render(<div>
-	<Icon name='wechat' onClick={fn}></Icon>
-</div>,document.getElementById('root'))
+export {default as Icon } from './icon/icon'
