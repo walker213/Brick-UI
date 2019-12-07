ef923290fc9750e051600d128b0aa6e3da65827c
walker213 <921254299@qq.com>
2019-12-07 17:10:50 +0800

    判断Layout的children是否有Sider再确定不同布局方向

--- lib/components/layout/index.tsx
+++ lib/components/layout/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import classnames from "classnames";
+import { addClassPrefixHOF } from "../../utils/classes";
 import Header from "./header";
 import Footer from "./footer";
 import Content from "./content";
 import Sider from "./sider";
-import './index.scss'
+import "./index.scss";
+
+const acp = addClassPrefixHOF('bui-layout');
 
 interface Props extends React.HTMLAttributes<HTMLElement> {}
 
@@ -16,10 +19,18 @@ type Fhp<T> = React.FunctionComponent<T> & {
 };
 
 const Layout: Fhp<Props> = props => {
-  const { className, ...restProps } = props;
+  const { className, children, ...restProps } = props;
+  const hasAside =
+    Array.isArray(children) &&
+    children.some(item => (item as ReactElement).type === Sider);
   return (
-    <section className={classnames(className, "bui-layout")} {...restProps}>
-      {props.children}
+    <section
+      className={classnames(className, acp(), {
+        [acp('hasAside')]: hasAside
+      })}
+      {...restProps}
+    >
+      {children}
     </section>
   );
 };
