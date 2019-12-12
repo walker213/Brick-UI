a15d8b0d34dbe379a7d6f149ee45744317f61e6c
walker213 <921254299@qq.com>
2019-12-13 02:51:44 +0800

    add more iconExample

--- lib/components/icon/iconExample.tsx
+++ lib/components/icon/iconExample.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Icon } from "../../index";
+import './iconExample.scss';
+
+const { svgNames } = require("./importIcon.js");
 
 const IconExample: React.FunctionComponent = () => {
   return (
-    <div>
-      <Icon name="swap"></Icon>
+    <div className="bui-iconExample">
+      {svgNames.map((name: string) => (
+        <div key={name}>
+          <Icon name={name}></Icon>
+          <span>{name}</span>
+        </div>
+      ))}
+      {/* <Icon name="swap"></Icon> */}
     </div>
   );
 };
